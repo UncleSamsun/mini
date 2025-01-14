7445661327d2c49f4daebdb3f0baa9549e91803d
김민준 <alswns5620@naver.com>
2025-01-14 10:40:51 +0900

    initialized commit

---
+++ src/test/java/io_groom/mini/MiniApplicationTests.java
@@ -0,0 +1,13 @@
+package io_groom.mini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
